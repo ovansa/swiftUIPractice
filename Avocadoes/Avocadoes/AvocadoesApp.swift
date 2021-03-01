7908e4da13c117d51463a1e7251e13a53bcac378
ovansa <aminmuhammad18@gmail.com>
2021-03-01 08:36:52 +0100

    Added setup files for Avocado app

---
+++ Avocadoes/Avocadoes/AvocadoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoesApp.swift
+//  Avocadoes
+//
+//  Created by Muhammed Ibrahim on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
